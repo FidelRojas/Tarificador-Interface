98a4dc79404fc2df4563ea877b2720eb43fe1e1e
Miguel Campero <miguelcamp@hotmail.com>
2020-05-25 23:20:40 -0400

    TelefonoTest

---
+++ tarificador/src/main/java/main/Telefono.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Telefono {
+	private String numero;
+	private String titular;
+	private String plan;
+	
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	public String getPlan() {
+		return plan;
+	}
+	public void setPlan(String plan) {
+		this.plan = plan;
+	}
+
+
+}
