3c29a6d9cbf7e9402e483b1da40cfd757ec37027
Miguel Campero <miguelcamp@hotmail.com>
2020-06-08 22:29:15 -0400

    Central con funciones pendientes

--- tarificador/src/main/java/tarificador/Central.java
+++ tarificador/src/main/java/tarificador/Central.java
@@ -5,16 +5,35 @@ import java.util.HashMap;
 public class Central {
 	HashMap<String, String> configuraciones;
 	private TarificadorBoundary tarificador;
+	private RegistroCDR[] CDRsCargados;
 	public Central() {
 		tarificador = new Tarificador();
 		ListaClientes LC= ListaClientes.getInstance();
 		configuraciones = new HashMap<String, String>();
 		configuraciones.put("persistencia", "guardarATxt");
 	}
-	
+	public void cargarCDRsDesdeTexto(String path) {
+//		TODO
+		
+	}
 	public double tarificarCDR(RegistroCDR registro) {
 		tarificador.setRegistro(registro);
 		return tarificador.calcularCostoLlamada();
+	}
+	
+	public void tarificarUnaListaDeCDRs(RegistroCDR [] registros) {
+		for(RegistroCDR registro : registros) {
+			tarificador.setRegistro(registro);
+			tarificador.calcularCostoLlamada();
+		}
+	}
+	
+	public RegistroCDR[] tarificarCDRsCargados() {
+		for(RegistroCDR registro : CDRsCargados) {
+			tarificador.setRegistro(registro);
+			tarificador.calcularCostoLlamada();
+		}
+		return CDRsCargados;
 	}
 	
 	public double facturarCliente(Cliente cliente) {
@@ -28,5 +47,15 @@ public class Central {
 
 	}
 	
+	public void guardarResultados() {
+		if(configuraciones.get("persistencia")== "guardarATxt") {
+			
+		}
+		else if(configuraciones.get("persistencia")=="guardarABaseDatos") {
+			
+		}
+	}
+	
+
 	
 }
