521cec7f7cee7baf1d57cfa7adc699f8d7aa10e2
Allen Vargas <allenbfox@gmail.com>
2020-06-16 03:00:16 -0400

    Facturadora interface

--- tarificador/src/main/java/Main.java
+++ tarificador/src/main/java/Main.java
@@ -1,9 +1,11 @@
 import interfaces.Interface;
+import interfaces.InterfaceFacturadora;
 import tarificador.Central;
 
 public class Main {
 	public static void main(String[] args) {
-		Central central = new Central();
-		Interface.run(central);
+		//Central central = new Central();
+		//Interface.run(central);
+		InterfaceFacturadora.run();
 	}
 }
