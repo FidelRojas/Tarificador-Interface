d80eead0b5f6b4d141cdf8aadaec2345f675e0b9
Allen Vargas <allenbfox@gmail.com>
2020-06-16 09:39:19 -0400

    Refactorizacion de fichero no usados

--- tarificador/src/main/java/Main.java
+++ tarificador/src/main/java/Main.java
@@ -1,11 +1,9 @@
 import interfaces.Interface;
-import interfaces.InterfaceFacturadora;
 import tarificador.Central;
 
 public class Main {
 	public static void main(String[] args) {
 		Central central = new Central();
 		Interface.run(central);
-		//InterfaceFacturadora.run();
 	}
 }
